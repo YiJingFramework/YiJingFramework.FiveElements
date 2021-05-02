cc75f214e773e365e46fd8267b1fe33a7ba04e71
yueyinqiu <lujiaqi2003@126.com>
2021-05-02 17:40:22 +0800

    fe

---
+++ YiJingFramework.FiveElements/YiJingFramework.FiveElements/FiveElement.cs
@@ -0,0 +1,246 @@
+﻿using System;
+using System.Diagnostics;
+using System.Diagnostics.CodeAnalysis;
+
+namespace YiJingFramework.FiveElements
+{
+    /// <summary>
+    /// 五行。
+    /// An element of the five elements.
+    /// </summary>
+    public struct FiveElement : IComparable<FiveElement>, IEquatable<FiveElement>
+    {
+        private readonly int int32Value;
+        private FiveElement(int int32ValueNotSmallerThanZero)
+        {
+            Debug.Assert(int32ValueNotSmallerThanZero >= 0);
+            this.int32Value = int32ValueNotSmallerThanZero % 5;
+        }
+
+        #region creating
+        /// <summary>
+        /// 木。
+        /// Wood.
+        /// </summary>
+        public static FiveElement Wood => default; // => new FiveElement(0);
+        /// <summary>
+        /// 火。
+        /// Fire.
+        /// </summary>
+        public static FiveElement Fire => new FiveElement(1);
+        /// <summary>
+        /// 土。
+        /// Earth.
+        /// </summary>
+        public static FiveElement Earth => new FiveElement(2);
+        /// <summary>
+        /// 金。
+        /// Metal.
+        /// </summary>
+        public static FiveElement Metal => new FiveElement(3);
+        /// <summary>
+        /// 水。
+        /// Water.
+        /// </summary>
+        public static FiveElement Water => new FiveElement(4);
+        #endregion
+
+        #region converting
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        public override string ToString()
+        {
+            return this.int32Value switch {
+                0 => "Wood",
+                1 => "Fire",
+                2 => "Earth",
+                3 => "Metal",
+                _ => "Water" // 4 => "Water"
+            };
+        }
+
+        /// <summary>
+        /// 从字符串转换。
+        /// Convert from a string.
+        /// </summary>
+        /// <param name="s">
+        /// 字符串。
+        /// The string.
+        /// </param>
+        /// <param name="result">
+        /// 结果。
+        /// The result.
+        /// </param>
+        /// <returns>
+        /// 一个指示转换成功与否的值。
+        /// A value indicates whether it has been successfully converted or not.
+        /// </returns>
+        public static bool TryParse(
+            [NotNullWhen(true)] string? s,
+            [MaybeNullWhen(false)] out FiveElement result)
+        {
+            switch (s?.Trim()?.ToLower())
+            {
+                case "wood":
+                    result = Wood;
+                    return true;
+                case "fire":
+                    result = Fire;
+                    return true;
+                case "earth":
+                    result = Earth;
+                    return true;
+                case "metal":
+                    result = Metal;
+                    return true;
+                case "water":
+                    result = Water;
+                    return true;
+                default:
+                    result = default;
+                    return false;
+            }
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="fiveElement"></param>
+        public static explicit operator int(FiveElement fiveElement)
+            => fiveElement.int32Value;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="value"></param>
+        public static explicit operator FiveElement(int value)
+            => new FiveElement(value % 5 + 5);
+        #endregion
+
+        #region comparing
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="other"></param>
+        /// <returns></returns>
+        public int CompareTo(FiveElement other)
+        {
+            return this.int32Value.CompareTo(other.int32Value);
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="other"></param>
+        /// <returns></returns>
+        public bool Equals(FiveElement other)
+        {
+            return this.int32Value.Equals(other.int32Value);
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="obj"></param>
+        /// <returns></returns>
+        public override bool Equals(object? obj)
+        {
+            if (obj is not FiveElement other)
+                return false;
+            return this.int32Value.Equals(other.int32Value);
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        public override int GetHashCode()
+        {
+            return this.int32Value.GetHashCode();
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="left"></param>
+        /// <param name="right"></param>
+        /// <returns></returns>
+        public static bool operator ==(FiveElement left, FiveElement right)
+            => left.int32Value == right.int32Value;
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="left"></param>
+        /// <param name="right"></param>
+        /// <returns></returns>
+        public static bool operator !=(FiveElement left, FiveElement right)
+            => left.int32Value != right.int32Value;
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="left"></param>
+        /// <param name="right"></param>
+        /// <returns></returns>
+        public static bool operator <(FiveElement left, FiveElement right)
+            => left.int32Value < right.int32Value;
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="left"></param>
+        /// <param name="right"></param>
+        /// <returns></returns>
+        public static bool operator <=(FiveElement left, FiveElement right)
+            => left.int32Value <= right.int32Value;
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="left"></param>
+        /// <param name="right"></param>
+        /// <returns></returns>
+        public static bool operator >(FiveElement left, FiveElement right)
+            => left.int32Value > right.int32Value;
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="left"></param>
+        /// <param name="right"></param>
+        /// <returns></returns>
+        public static bool operator >=(FiveElement left, FiveElement right)
+            => left.int32Value >= right.int32Value;
+        #endregion
+
+        #region generating and overcoming
+        /// <summary>
+        /// 获取与另一五行之间的关系。
+        /// Get the relationship with another element.
+        /// </summary>
+        /// <param name="another">
+        /// 另一五行。
+        /// The another element.
+        /// </param>
+        /// <returns>
+        /// 关系。
+        /// The relationship.
+        /// </returns>
+        public FiveElementsRelationship GetRelationship(FiveElement another)
+        {
+            return (FiveElementsRelationship)((another.int32Value - this.int32Value + 5) % 5);
+        }
+        /// <summary>
+        /// 通过五行关系获取另一五行。
+        /// Get another element with the relationship.
+        /// </summary>
+        /// <param name="relation">
+        /// 关系。
+        /// The relationship.
+        /// </param>
+        /// <returns>
+        /// 另一五行。
+        /// The another element.
+        /// </returns>
+        public FiveElement GetElement(FiveElementsRelationship relation)
+        {
+            return new FiveElement(this.int32Value + (int)relation);
+        }
+        #endregion
+    }
+}
---
+++ YiJingFramework.FiveElements/YiJingFramework.FiveElementsTests/FiveElementTests.cs
@@ -0,0 +1,197 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using YiJingFramework.FiveElements;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YiJingFramework.FiveElements.Tests
+{
+    [TestClass()]
+    public class FiveElementTests
+    {
+        [TestMethod()]
+        public void ConvertingTest()
+        {
+            Assert.AreEqual(0, (int)FiveElement.Wood);
+            Assert.AreEqual(1, (int)FiveElement.Fire);
+            Assert.AreEqual(2, (int)FiveElement.Earth);
+            Assert.AreEqual(3, (int)FiveElement.Metal);
+            Assert.AreEqual(4, (int)FiveElement.Water);
+
+            Assert.AreEqual("Wood", FiveElement.Wood.ToString());
+            Assert.AreEqual("Fire", FiveElement.Fire.ToString());
+            Assert.AreEqual("Earth", FiveElement.Earth.ToString());
+            Assert.AreEqual("Metal", FiveElement.Metal.ToString());
+            Assert.AreEqual("Water", FiveElement.Water.ToString());
+
+            Assert.IsTrue(FiveElement.TryParse("Wood", out FiveElement r));
+            Assert.AreEqual(FiveElement.Wood, r);
+            Assert.IsTrue(FiveElement.TryParse("\t\t  Wood\r\n", out r));
+            Assert.AreEqual(FiveElement.Wood, r);
+            Assert.IsTrue(FiveElement.TryParse("Fire", out r));
+            Assert.AreEqual(FiveElement.Fire, r);
+            Assert.IsTrue(FiveElement.TryParse("\t   \t  fire\r\n", out r));
+            Assert.AreEqual(FiveElement.Fire, r);
+            Assert.IsTrue(FiveElement.TryParse("earth", out r));
+            Assert.AreEqual(FiveElement.Earth, r);
+            Assert.IsTrue(FiveElement.TryParse("Metal", out r));
+            Assert.AreEqual(FiveElement.Metal, r);
+            Assert.IsTrue(FiveElement.TryParse("  water ", out r));
+            Assert.AreEqual(FiveElement.Water, r);
+            Assert.IsFalse(FiveElement.TryParse("false", out _));
+            Assert.IsFalse(FiveElement.TryParse(null, out _));
+
+            var elements = new FiveElement[] {
+                FiveElement.Wood,
+                FiveElement.Fire,
+                FiveElement.Earth,
+                FiveElement.Metal,
+                FiveElement.Water
+            };
+            for (int i = -1000, j = 0; i < 1000; i++)
+            {
+                Assert.AreEqual(elements[j], (FiveElement)i);
+                j++;
+                if (j == 5)
+                    j = 0;
+            }
+        }
+
+        [TestMethod()]
+        public void ComparingTest()
+        {
+            Random r = new Random();
+            for (int i = 0; i < 20000; i++)
+            {
+                var fir = (r.Next(-10000, 9999) % 5 + 5) % 5;
+                var sec = (r.Next(-10000, 9999) % 5 + 5) % 5;
+                var firF = (FiveElement)fir;
+                var secF = (FiveElement)sec;
+                if (fir == sec)
+                {
+                    Assert.AreEqual(0, firF.CompareTo(secF));
+                    Assert.AreEqual(0, secF.CompareTo(firF));
+                    Assert.AreEqual(true, firF.Equals(secF));
+                    Assert.AreEqual(true, secF.Equals(firF));
+                    Assert.AreEqual(true, firF.Equals((object)secF));
+                    Assert.AreEqual(true, secF.Equals((object)firF));
+                    Assert.AreEqual(firF.GetHashCode(), secF.GetHashCode());
+                    Assert.AreEqual(true, firF == secF);
+                    Assert.AreEqual(true, secF == firF);
+                    Assert.AreEqual(false, firF != secF);
+                    Assert.AreEqual(false, secF != firF);
+                    Assert.AreEqual(false, firF < secF);
+                    Assert.AreEqual(false, secF < firF);
+                    Assert.AreEqual(false, firF > secF);
+                    Assert.AreEqual(false, secF > firF);
+                    Assert.AreEqual(true, firF <= secF);
+                    Assert.AreEqual(true, secF <= firF);
+                    Assert.AreEqual(true, firF >= secF);
+                    Assert.AreEqual(true, secF >= firF);
+                }
+
+                else if (fir < sec)
+                {
+                    Assert.AreEqual(-1, firF.CompareTo(secF));
+                    Assert.AreEqual(1, secF.CompareTo(firF));
+                    Assert.AreEqual(false, firF.Equals(secF));
+                    Assert.AreEqual(false, secF.Equals(firF));
+                    Assert.AreEqual(false, firF.Equals((object)secF));
+                    Assert.AreEqual(false, secF.Equals((object)firF));
+                    Assert.AreNotEqual(firF.GetHashCode(), secF.GetHashCode());
+                    Assert.AreEqual(false, firF == secF);
+                    Assert.AreEqual(false, secF == firF);
+                    Assert.AreEqual(true, firF != secF);
+                    Assert.AreEqual(true, secF != firF);
+                    Assert.AreEqual(true, firF < secF);
+                    Assert.AreEqual(false, secF < firF);
+                    Assert.AreEqual(false, firF > secF);
+                    Assert.AreEqual(true, secF > firF);
+                    Assert.AreEqual(true, firF <= secF);
+                    Assert.AreEqual(false, secF <= firF);
+                    Assert.AreEqual(false, firF >= secF);
+                    Assert.AreEqual(true, secF >= firF);
+                }
+
+                else // fir > sec
+                {
+                    Assert.AreEqual(1, firF.CompareTo(secF));
+                    Assert.AreEqual(-1, secF.CompareTo(firF));
+                    Assert.AreEqual(false, firF.Equals(secF));
+                    Assert.AreEqual(false, secF.Equals(firF));
+                    Assert.AreEqual(false, firF.Equals((object)secF));
+                    Assert.AreEqual(false, secF.Equals((object)firF));
+                    Assert.AreNotEqual(firF.GetHashCode(), secF.GetHashCode());
+                    Assert.AreEqual(false, firF == secF);
+                    Assert.AreEqual(false, secF == firF);
+                    Assert.AreEqual(true, firF != secF);
+                    Assert.AreEqual(true, secF != firF);
+                    Assert.AreEqual(false, firF < secF);
+                    Assert.AreEqual(true, secF < firF);
+                    Assert.AreEqual(true, firF > secF);
+                    Assert.AreEqual(false, secF > firF);
+                    Assert.AreEqual(false, firF <= secF);
+                    Assert.AreEqual(true, secF <= firF);
+                    Assert.AreEqual(true, firF >= secF);
+                    Assert.AreEqual(false, secF >= firF);
+                }
+                Assert.AreEqual(false, firF.Equals(null));
+                Assert.AreEqual(false, secF.Equals(new object()));
+            }
+        }
+        [TestMethod()]
+        public void GeneratingAndOvercomingTest()
+        {
+            Assert.AreEqual(FiveElementsRelationship.GeneratedByMe,
+                FiveElement.Wood.GetRelationship(FiveElement.Fire));
+            Assert.AreEqual(FiveElementsRelationship.GeneratedByMe,
+                FiveElement.Fire.GetRelationship(FiveElement.Earth));
+            Assert.AreEqual(FiveElementsRelationship.GeneratedByMe,
+                FiveElement.Earth.GetRelationship(FiveElement.Metal));
+            Assert.AreEqual(FiveElementsRelationship.GeneratedByMe,
+                FiveElement.Metal.GetRelationship(FiveElement.Water));
+            Assert.AreEqual(FiveElementsRelationship.GeneratedByMe,
+                FiveElement.Water.GetRelationship(FiveElement.Wood));
+
+            Assert.AreEqual(FiveElementsRelationship.GeneratingMe,
+                FiveElement.Wood.GetRelationship(FiveElement.Water));
+            Assert.AreEqual(FiveElementsRelationship.GeneratingMe,
+                FiveElement.Water.GetRelationship(FiveElement.Metal));
+            Assert.AreEqual(FiveElementsRelationship.GeneratingMe,
+                FiveElement.Metal.GetRelationship(FiveElement.Earth));
+            Assert.AreEqual(FiveElementsRelationship.GeneratingMe,
+                FiveElement.Earth.GetRelationship(FiveElement.Fire));
+            Assert.AreEqual(FiveElementsRelationship.GeneratingMe,
+                FiveElement.Fire.GetRelationship(FiveElement.Wood));
+
+            for (int i = 0; i < 5; i++)
+            {
+                var woodP = (FiveElement)i;
+                var fireP = woodP.GetElement(FiveElementsRelationship.GeneratedByMe);
+                var earthP = fireP.GetElement(FiveElementsRelationship.GeneratedByMe);
+                var metalP = earthP.GetElement(FiveElementsRelationship.GeneratedByMe);
+                var waterP = metalP.GetElement(FiveElementsRelationship.GeneratedByMe);
+
+                Assert.AreEqual(FiveElementsRelationship.GeneratedByMe,
+                    woodP.GetRelationship(fireP));
+                Assert.AreEqual(FiveElementsRelationship.OvercameByMe,
+                    woodP.GetRelationship(earthP));
+                Assert.AreEqual(FiveElementsRelationship.OvercomingMe,
+                    woodP.GetRelationship(metalP));
+                Assert.AreEqual(FiveElementsRelationship.GeneratingMe,
+                    woodP.GetRelationship(waterP));
+
+                Assert.AreEqual(fireP,
+                    woodP.GetElement(FiveElementsRelationship.GeneratedByMe));
+                Assert.AreEqual(earthP,
+                    woodP.GetElement(FiveElementsRelationship.OvercameByMe));
+                Assert.AreEqual(metalP,
+                    woodP.GetElement(FiveElementsRelationship.OvercomingMe));
+                Assert.AreEqual(waterP,
+                    woodP.GetElement(FiveElementsRelationship.GeneratingMe));
+            }
+        }
+    }
+}
